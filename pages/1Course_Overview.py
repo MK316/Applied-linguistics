3091da5659db8ddc35af0a3226e9e54f4743089f
MK <99416359+MK316@users.noreply.github.com>
2026-01-14 17:32:03 +0900

    Update 1Course_Overview.py

--- pages/1Course_Overview.py
+++ pages/1Course_Overview.py
@@ -1,22 +1,16 @@
 import streamlit as st
-import base64
-from pathlib import Path
-
-st.set_page_config(layout="wide")
 
 tab1, tab2, tab3 = st.tabs(["Course Overview", "Online Links", "TBA"])
 
 with tab1:
     st.markdown("### 📄 Course Overview (PDF)")
 
-    pdf_path = Path("https://raw.githubusercontent.com/MK316/Applied-linguistics/main/data/S26-appling.pdf")  # ← PDF 파일 경로
-    pdf_bytes = pdf_path.read_bytes()
-    b64_pdf = base64.b64encode(pdf_bytes).decode("utf-8")
+    pdf_url = "https://raw.githubusercontent.com/MK316/Applied-linguistics/main/data/S26-appling.pdf"
 
     st.markdown(
         f"""
         <iframe
-            src="data:application/pdf;base64,{b64_pdf}"
+            src="{pdf_url}"
             width="100%"
             height="900"
             style="border: none;"
