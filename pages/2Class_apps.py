58d5d8db222b730e62cae755057c8e5a2a1fb482
MK <99416359+MK316@users.noreply.github.com>
2026-01-14 16:24:51 +0900

    Update 2Class_apps.py

--- pages/2Class_apps.py
+++ pages/2Class_apps.py
@@ -18,7 +18,7 @@ def create_wordcloud(text):
     return wordcloud
 
 # Streamlit tabs
-tabs = st.tabs(["📈 QR", "⏳ Timer", "👥 Grouping", "🐤 GoogleSheet","🔊 Text-to-Speech", "🎨 Drawing"])
+tabs = st.tabs(["📈 QR", "⏳ Timer", "👥 Grouping", "🐤 GS","🔊 TTS", "🎨 Drawing"])
 
 # QR Code tab
 with tabs[0]:
