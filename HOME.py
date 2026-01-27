09c0db066f3d8acc57f8f3f418ccaaf23c38cf75
MK <99416359+MK316@users.noreply.github.com>
2026-01-27 13:16:05 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -1,4 +1,14 @@
+https://github.com/MK316/Applied-linguistics/blob/main/images/AL-bg1.png
+
 import streamlit as st
 
 st.markdown("#### Applied Linguistics (Spring 2026)")
 st.caption("To be updated soon")
+
+IMAGE_URL = "https://raw.githubusercontent.com/MK316/Applied-linguistics/main/images/to/AL-bg1.png"
+
+st.image(
+    IMAGE_URL,
+    use_container_width=True
+)
+
