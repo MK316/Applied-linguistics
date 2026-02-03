1cbf66236c61ee456d18e53b12e7be66943ac625
MK <99416359+MK316@users.noreply.github.com>
2026-02-03 21:39:13 +0900

    Update 2🍰_Class_apps.py

--- pages/2🍰_Class_apps.py
+++ pages/2🍰_Class_apps.py
@@ -18,7 +18,7 @@ def create_wordcloud(text):
     return wordcloud
 
 # Streamlit tabs
-tabs = st.tabs(["Blackboard", "📈 QR", "⏳ Timer", "👥 Grouping", "🐤 GS","🔊 TTS", "🎨 Drawing"])
+tabs = st.tabs(["Blackboard", "📈 QR", "⏳ Timer", "🐤 GS","🔊 TTS", "🎨 Drawing", "👥 Grouping", ])
 
 # QR Code tab
 with tabs[0]:
@@ -114,8 +114,121 @@ with tabs[2]:
         <iframe src="{huggingface_space_url}" width="100%" height="600px" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     """, height=600)
 
-# Grouping tab
+
+
 with tabs[3]:
+    st.markdown("#### Google Sheet to share for Class Activities")
+    st.markdown("""
+    + Working group (1st week)
+
+    """)
+
+    st.markdown("---")
+    # Custom styled HTML button
+    button_html = """
+        <style>
+            .custom-button {
+                background-color: #003366;
+                color: white;
+                padding: 10px 20px;
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+                font-size: 16px;
+                border: none;
+                border-radius: 8px;
+                cursor: pointer;
+            }
+            .custom-button:hover {
+                background-color: #002244;
+            }
+        </style>
+        <a href="https://docs.google.com/spreadsheets/d/1JsW8sRnnVAMwgUSpXK3ygO0YvJqzLC5ZucOF_523Lzg/edit?usp=sharing" target="_blank">
+            <button class="custom-button">🎯 Click: Go to Google Sheet</button>
+        </a>
+    """
+    st.markdown(button_html, unsafe_allow_html=True)
+
+
+# Text-to-Speech tab
+with tabs[4]:
+    st.subheader("Text-to-Speech Converter (using Google TTS)")
+    text_input = st.text_area("Enter the text you want to convert to speech:")
+    language = st.selectbox("Choose a language: 🇰🇷 🇺🇸 🇬🇧 🇷🇺 🇫🇷 🇪🇸 🇯🇵 ", ["Korean", "English (American)", "English (British)", "Russian", "Spanish", "French", "Japanese"])
+
+    tts_button = st.button("Convert Text to Speech")
+    
+    if tts_button and text_input:
+        # Map human-readable language selection to language codes and optionally to TLDs for English
+        lang_codes = {
+            "Korean": ("ko", None),
+            "English (American)": ("en", 'com'),
+            "English (British)": ("en", 'co.uk'),
+            "Russian": ("ru", None),
+            "Spanish": ("es", None),
+            "French": ("fr", None),
+            "Chinese": ("zh-CN", None),
+            "Japanese": ("ja", None)
+        }
+        language_code, tld = lang_codes[language]
+
+        # Assuming you have a version of gTTS that supports tld or you have modified it:
+        # This check ensures that the tld parameter is only used when not None.
+        if tld:
+            tts = gTTS(text=text_input, lang=language_code, tld=tld, slow=False)
+        else:
+            tts = gTTS(text=text_input, lang=language_code, slow=False)
+        
+        speech = io.BytesIO()
+        tts.write_to_fp(speech)
+        speech.seek(0)
+
+        # Display the audio file
+        st.audio(speech.getvalue(), format='audio/mp3')
+    st.markdown("---")
+    st.caption("🇺🇸 English text: Teacher-designed coding applications create tailored learning experiences, making complex concepts easier to understand through interactive and adaptive tools. They enhance engagement, provide immediate feedback, and support active learning.")
+    st.caption("🇰🇷 Korean text: 교사가 직접 만든 코딩 기반 애플리케이션은 학습자의 필요에 맞춘 학습 경험을 제공하고, 복잡한 개념을 쉽게 이해하도록 돕습니다. 또한 학습 몰입도를 높이고 즉각적인 피드백을 제공하며, 능동적인 학습을 지원합니다.")
+    st.caption("🇫🇷 French: Les applications de codage conçues par les enseignants offrent une expérience d'apprentissage personnalisée, rendant les concepts complexes plus faciles à comprendre grâce à des outils interactifs et adaptatifs. Elles améliorent l'engagement, fournissent un retour immédiat et soutiennent l'apprentissage actif.")
+    st.caption("🇷🇺 Russian: Созданные учителями кодированные приложения предлагают персонализированный опыт обучения, упрощая понимание сложных концепций с помощью интерактивных и адаптивных инструментов. Они повышают вовлеченность, предоставляют мгновенную обратную связь и поддерживают активное обучение.")
+    st.caption("🇨🇳 Chinese: 由教师设计的编程应用程序为学习者提供个性化的学习体验，通过互动和适应性工具使复杂的概念更容易理解。它们增强学习参与度，提供即时反馈，并支持主动学习。")
+    st.caption("🇯🇵 Japanese: 教師が設計したコーディングアプリケーションは、学習者のニーズに合わせた学習体験を提供し、複雑な概念をインタラクティブで適応性のあるツールを通じて理解しやすくします。また、学習への集中力を高め、即時フィードバックを提供し、主体的な学習をサポートします。")
+
+with tabs[5]:
+    st.caption("Use the canvas below to draw freely. You can change the stroke width and color.")
+
+   # Place Stroke Width, Stroke Color, and Background Color in the same row
+    col1, col2, col3 = st.columns([1, 1, 1])
+
+    with col1:
+        stroke_width = st.slider("✏️ Stroke Width", 1, 10, 5)
+    with col2:
+        stroke_color = st.color_picker("🖌 Stroke Color", "#000000")
+    with col3:
+        bg_color = st.color_picker("🖼 Background Color", "#FFFFFF")
+
+    # Initialize session state for clearing
+    if "clear_canvas" not in st.session_state:
+        st.session_state["clear_canvas"] = False
+
+    # Create the canvas (Unique key prevents duplication)
+    canvas_result = st_canvas(
+        fill_color="rgba(255, 165, 0, 0.3)",  
+        stroke_width=stroke_width,
+        stroke_color=stroke_color,
+        background_color=bg_color,
+        height=400,
+        width=600,
+        drawing_mode="freedraw",
+        key="main_canvas" if not st.session_state["clear_canvas"] else "new_canvas"
+    )
+
+    # Clear Canvas button
+    if st.button("🗑️ Clear Canvas"):
+        st.session_state["clear_canvas"] = not st.session_state["clear_canvas"]
+        st.rerun()  # This forces Streamlit to reload and clear the drawing
+
+# Grouping tab
+with tabs[6]:
     st.subheader("👥 Grouping Tool")
     st.caption("Your CSV should have at least the columns `Course` and `Name_ori`.")
 
@@ -192,114 +305,3 @@ with tabs[3]:
         st.error("The file must contain both `Course` and `Name_ori` columns.")
 
 #--------Tab 3
-
-with tabs[4]:
-    st.markdown("#### Google Sheet to share for Class Activities")
-    st.markdown("""
-    + Working group (1st week)
-
-    """)
-
-    st.markdown("---")
-    # Custom styled HTML button
-    button_html = """
-        <style>
-            .custom-button {
-                background-color: #003366;
-                color: white;
-                padding: 10px 20px;
-                text-align: center;
-                text-decoration: none;
-                display: inline-block;
-                font-size: 16px;
-                border: none;
-                border-radius: 8px;
-                cursor: pointer;
-            }
-            .custom-button:hover {
-                background-color: #002244;
-            }
-        </style>
-        <a href="https://docs.google.com/spreadsheets/d/1JsW8sRnnVAMwgUSpXK3ygO0YvJqzLC5ZucOF_523Lzg/edit?usp=sharing" target="_blank">
-            <button class="custom-button">🎯 Click: Go to Google Sheet</button>
-        </a>
-    """
-    st.markdown(button_html, unsafe_allow_html=True)
-
-
-# Text-to-Speech tab
-with tabs[5]:
-    st.subheader("Text-to-Speech Converter (using Google TTS)")
-    text_input = st.text_area("Enter the text you want to convert to speech:")
-    language = st.selectbox("Choose a language: 🇰🇷 🇺🇸 🇬🇧 🇷🇺 🇫🇷 🇪🇸 🇯🇵 ", ["Korean", "English (American)", "English (British)", "Russian", "Spanish", "French", "Japanese"])
-
-    tts_button = st.button("Convert Text to Speech")
-    
-    if tts_button and text_input:
-        # Map human-readable language selection to language codes and optionally to TLDs for English
-        lang_codes = {
-            "Korean": ("ko", None),
-            "English (American)": ("en", 'com'),
-            "English (British)": ("en", 'co.uk'),
-            "Russian": ("ru", None),
-            "Spanish": ("es", None),
-            "French": ("fr", None),
-            "Chinese": ("zh-CN", None),
-            "Japanese": ("ja", None)
-        }
-        language_code, tld = lang_codes[language]
-
-        # Assuming you have a version of gTTS that supports tld or you have modified it:
-        # This check ensures that the tld parameter is only used when not None.
-        if tld:
-            tts = gTTS(text=text_input, lang=language_code, tld=tld, slow=False)
-        else:
-            tts = gTTS(text=text_input, lang=language_code, slow=False)
-        
-        speech = io.BytesIO()
-        tts.write_to_fp(speech)
-        speech.seek(0)
-
-        # Display the audio file
-        st.audio(speech.getvalue(), format='audio/mp3')
-    st.markdown("---")
-    st.caption("🇺🇸 English text: Teacher-designed coding applications create tailored learning experiences, making complex concepts easier to understand through interactive and adaptive tools. They enhance engagement, provide immediate feedback, and support active learning.")
-    st.caption("🇰🇷 Korean text: 교사가 직접 만든 코딩 기반 애플리케이션은 학습자의 필요에 맞춘 학습 경험을 제공하고, 복잡한 개념을 쉽게 이해하도록 돕습니다. 또한 학습 몰입도를 높이고 즉각적인 피드백을 제공하며, 능동적인 학습을 지원합니다.")
-    st.caption("🇫🇷 French: Les applications de codage conçues par les enseignants offrent une expérience d'apprentissage personnalisée, rendant les concepts complexes plus faciles à comprendre grâce à des outils interactifs et adaptatifs. Elles améliorent l'engagement, fournissent un retour immédiat et soutiennent l'apprentissage actif.")
-    st.caption("🇷🇺 Russian: Созданные учителями кодированные приложения предлагают персонализированный опыт обучения, упрощая понимание сложных концепций с помощью интерактивных и адаптивных инструментов. Они повышают вовлеченность, предоставляют мгновенную обратную связь и поддерживают активное обучение.")
-    st.caption("🇨🇳 Chinese: 由教师设计的编程应用程序为学习者提供个性化的学习体验，通过互动和适应性工具使复杂的概念更容易理解。它们增强学习参与度，提供即时反馈，并支持主动学习。")
-    st.caption("🇯🇵 Japanese: 教師が設計したコーディングアプリケーションは、学習者のニーズに合わせた学習体験を提供し、複雑な概念をインタラクティブで適応性のあるツールを通じて理解しやすくします。また、学習への集中力を高め、即時フィードバックを提供し、主体的な学習をサポートします。")
-
-with tabs[6]:
-    st.caption("Use the canvas below to draw freely. You can change the stroke width and color.")
-
-   # Place Stroke Width, Stroke Color, and Background Color in the same row
-    col1, col2, col3 = st.columns([1, 1, 1])
-
-    with col1:
-        stroke_width = st.slider("✏️ Stroke Width", 1, 10, 5)
-    with col2:
-        stroke_color = st.color_picker("🖌 Stroke Color", "#000000")
-    with col3:
-        bg_color = st.color_picker("🖼 Background Color", "#FFFFFF")
-
-    # Initialize session state for clearing
-    if "clear_canvas" not in st.session_state:
-        st.session_state["clear_canvas"] = False
-
-    # Create the canvas (Unique key prevents duplication)
-    canvas_result = st_canvas(
-        fill_color="rgba(255, 165, 0, 0.3)",  
-        stroke_width=stroke_width,
-        stroke_color=stroke_color,
-        background_color=bg_color,
-        height=400,
-        width=600,
-        drawing_mode="freedraw",
-        key="main_canvas" if not st.session_state["clear_canvas"] else "new_canvas"
-    )
-
-    # Clear Canvas button
-    if st.button("🗑️ Clear Canvas"):
-        st.session_state["clear_canvas"] = not st.session_state["clear_canvas"]
-        st.rerun()  # This forces Streamlit to reload and clear the drawing
