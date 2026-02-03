1b90db8460291fc24a91540327883c6086343efa
MK <99416359+MK316@users.noreply.github.com>
2026-02-03 21:13:07 +0900

    Update 8Class_Notes.py

--- pages/8Class_Notes.py
+++ pages/8Class_Notes.py
@@ -1,11 +1,14 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
+st.set_page_config(layout="wide")
+
 def main():
     st.caption("💙 Class Notes: Google doc")
     st.write("➡️ Share your ideas.")
     # Padlet embed URL (you need to replace this with your actual Padlet embed URL)
-    padlet_url = "https://docs.google.com/document/d/1olgbyInMLUFaYPjt8G_V-tNtb1Kfhjts4gq4hxPYNmQ/edit?usp=sharing"
+    padlet_url = "https://docs.google.com/document/d/1olgbyInMLUFaYPjt8G_V-tNtb1Kfhjts4gq4hxPYNmQ/preview"
+
 
     # Create an iframe to embed the Padlet
     padlet_iframe = f"<iframe src='{padlet_url}' width='100%' height='600' frameborder='0' allow='autoplay'></iframe>"
