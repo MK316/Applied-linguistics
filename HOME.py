e3e248c15df225b758a6cfef5645a5a7280e717c
MK <99416359+MK316@users.noreply.github.com>
2026-02-03 19:17:11 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -3,7 +3,7 @@ import streamlit as st
 # url = "https://github.com/MK316/Applied-linguistics/blob/main/images/AL-bg1.png"
 
 st.markdown("#### Spring 2026")
-st.caption("To be updated soon")
+st.caption("This page is continuously updated.")
 
 IMAGE_URL = "https://raw.githubusercontent.com/MK316/Applied-linguistics/main/images/AL-bg1.png"
 
