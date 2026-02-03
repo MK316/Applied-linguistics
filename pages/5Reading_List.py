0c850b8797fda1418838d0f61ebb916369de54d8
MK <99416359+MK316@users.noreply.github.com>
2026-02-03 20:43:14 +0900

    Update 5Reading_List.py

--- pages/5Reading_List.py
+++ pages/5Reading_List.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
-st.markdown("[Li, Wei (2018)](https://www.researchgate.net/publication/323720294_Translanguaging_as_a_Practical_Theory_of_Language)")
+st.write("Reading list (To be updated continuously)")
+st.markdown("""
+  + Translanguaging: [Li, Wei (2018)](https://www.researchgate.net/publication/323720294_Translanguaging_as_a_Practical_Theory_of_Language)
+  """)
