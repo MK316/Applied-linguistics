dc796fe24760ea21ad77e3d8e298750aa0896017
MK <99416359+MK316@users.noreply.github.com>
2026-02-08 21:46:30 +0900

    Update 3📗_Reading_List.py

--- pages/3📗_Reading_List.py
+++ pages/3📗_Reading_List.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.write("Reading list (To be updated continuously)")
 st.markdown("""
-  + Translanguaging: [Li, Wei (2018)](https://www.researchgate.net/publication/323720294_Translanguaging_as_a_Practical_Theory_of_Language)
-  + Alignment issues: [Curry et al. (2025)](https://github.com/MK316/Applied-linguistics/raw/main/readings/02_Curry_etal_2025.pdf)
+  + Reading #1: Translanguaging [Li, Wei (2018)](https://www.researchgate.net/publication/323720294_Translanguaging_as_a_Practical_Theory_of_Language)
+  + Reading #2: Alignment issues [Curry et al. (2025)](https://github.com/MK316/Applied-linguistics/raw/main/readings/02_Curry_etal_2025.pdf)
   """)
