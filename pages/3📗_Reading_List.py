53b4609b0af46c0aa78f0f92a6958149acc249f1
MK <99416359+MK316@users.noreply.github.com>
2026-02-08 21:40:30 +0900

    Update 3📗_Reading_List.py

--- pages/3📗_Reading_List.py
+++ pages/3📗_Reading_List.py
@@ -3,4 +3,5 @@ import streamlit as st
 st.write("Reading list (To be updated continuously)")
 st.markdown("""
   + Translanguaging: [Li, Wei (2018)](https://www.researchgate.net/publication/323720294_Translanguaging_as_a_Practical_Theory_of_Language)
+  + Alignment issues: [Curry et al. (2025)(readings/02_Curry_etal_2025.pdf)
   """)
