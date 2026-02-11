c751e0619767b003720f6dc01205d7d1a0d7d226
MK <99416359+MK316@users.noreply.github.com>
2026-02-11 22:29:12 +0900

    Update 3📗_Reading_List.py

--- pages/3📗_Reading_List.py
+++ pages/3📗_Reading_List.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.markdown("### 🔖 Reading list")
 st.caption("To be updated continuously")
 st.markdown("""
+  📗 Spplementary book: An Introduction to Applied Linguistics (2007; 2nd ed.) by A. Davies, Edinburgh University Press.
   + Reading #1: Translanguaging [Li, Wei (2018)](https://www.researchgate.net/publication/323720294_Translanguaging_as_a_Practical_Theory_of_Language)
   + Reading #2: Alignment issues [Curry et al. (2025)](https://github.com/MK316/Applied-linguistics/raw/main/readings/02_Curry_etal_2025.pdf)
   """)
