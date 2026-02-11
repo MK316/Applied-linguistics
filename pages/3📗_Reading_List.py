14e3690865b34da145490ffe6abc5f068217d63c
MK <99416359+MK316@users.noreply.github.com>
2026-02-11 22:40:37 +0900

    Update 3📗_Reading_List.py

--- pages/3📗_Reading_List.py
+++ pages/3📗_Reading_List.py
@@ -6,4 +6,9 @@ st.markdown("""
   📗 Spplementary book: An Introduction to Applied Linguistics (2007; 2nd ed.) by A. Davies, Edinburgh University Press.
   + Reading #1: Translanguaging [Li, Wei (2018)](https://www.researchgate.net/publication/323720294_Translanguaging_as_a_Practical_Theory_of_Language)
   + Reading #2: Alignment issues [Curry et al. (2025)](https://github.com/MK316/Applied-linguistics/raw/main/readings/02_Curry_etal_2025.pdf)
+  + Reading #3: Jacob, S. R. & M. Warschauer (2018). Computational Thinking and Literacy. Journal of Computer Science Integration, 1(1).
+  + Reading #4: Jeon et al. (2025). Generative AI and its dilemmas: exploring AI from a translanguaging perspective. Applied Linguistics, 46, 709-717.
+  + Reading #5: [UNESCO](https://www.unesco.org/en/digital-education/artificial-intelligence) (2025) AI in education.
+  + Reading #6: 
+  
   """)
