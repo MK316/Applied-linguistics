39e3290cbe9a50d550bdba4ccf6aee660d5fe49a
MK <99416359+MK316@users.noreply.github.com>
2026-02-11 22:52:32 +0900

    Create 〽️_APP: Collaboration_Showcase.py

---
+++ pages/〽️_APP: Collaboration_Showcase.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.caption("Making learning tools and share codes")
+
+tab1, tab2, tab3 = st.tabs(["tab1","tab2","tab3"])
+
+with tab1:
+  st.write("Collaboration apps will be displayed here in time.")
+
+with tab2:
+  st.write("Collaboration apps will be displayed here in time.")
+
+with tab3:
+  st.write("Collaboration apps will be displayed here in time.")
