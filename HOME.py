474cf7bd334d1bd628c58dcf23907d6193c4187e
Alexwcjung <127401494+Alexwcjung@users.noreply.github.com>
2026-04-26 17:47:19 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -1,11 +1,10 @@
 import streamlit as st
 
+st.markdown("### ✅ 이미지 수정 테스트 중")
 st.markdown("#### Spring 2026")
 st.caption("This page is continuously updated.")
 
-IMAGE_URL = "https://raw.githubusercontent.com/Alexwcjung/Fun-English/main/a143182b-832c-4a27-87fb-74214eabb338.png?v=3"
-
 st.image(
-    IMAGE_URL,
+    "fun_english_new.png",
     use_container_width=True
 )
