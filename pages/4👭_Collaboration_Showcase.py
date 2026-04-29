b671d88c8faff339b6843db5090e4e5003a85c98
MK <99416359+MK316@users.noreply.github.com>
2026-04-29 09:46:22 +0900

    Update 4👭_Collaboration_Showcase.py

--- pages/4👭_Collaboration_Showcase.py
+++ pages/4👭_Collaboration_Showcase.py
@@ -27,8 +27,8 @@ with tab1:
         "Select a project": None,
         "Overview & guidelines": "https://raw.githubusercontent.com/MK316/Collaboration26/main/mini-project/readme.md",
         "Sample - Multi-language TTS": "https://raw.githubusercontent.com/MK316/Collaboration26/main/mini-project/sample.md",
-        "Pair 1 - Sentence by Sentence TTS": "https://github.com/MK316/Collaboration26/blob/main/mini-project/miniproject-sample.md",
-        "Pair 2 - Verb Learning": "https://github.com/JHC0531/Mini-project/blob/main/README.md",
+        "Pair 1 - Sentence by Sentence TTS": "https://github.com/MK316/Collaboration26/raw/main/mini-project/miniproject-sample.md",
+        "Pair 2 - Verb Learning": "https://github.com/JHC0531/Mini-project/raw/main/README.md",
         "Pair 3 - Project title": "https://raw.githubusercontent.com/USERNAME/REPO/main/project4.md",
     }
 
